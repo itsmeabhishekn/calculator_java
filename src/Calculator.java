5b6c36e05ac0efd88829c50890a2b46fbc4ee477
itsmeabhishekn <abhishekn4454@gmail.com>
2022-11-21 10:32:15 +0530

    added addition functionality

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,7 +10,18 @@ public class Calculator {
                 " Please enter the choice \n" + "1.Addition \n2.Subtraction \n3.Multiplication \n4.Division \n5.Mod "
         );
 
+        int ch = reader.nextInt();
 
+        switch (ch)
+        {
+            case 1:
+                int n1 = reader.nextInt();
+                int n2 = reader.nextInt();
+                System.out.println("the output is " +n1+n2);
+                break;
+
+
+        }
 
     }
 
